42ca2374702e3509fd46b557fe9eced57d91c484
Alex Mcintosh <56128988+638thrasher836@users.noreply.github.com>
2019-12-12 09:14:52 -0800

    coding prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (transform.position.x < -10)
-
+        {
             transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-       
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
+}
+        
